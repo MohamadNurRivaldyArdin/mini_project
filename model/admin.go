5c69e183fb0283c2d7e63e587954491ddb2151fa
MohamadNurRivaldyArdin <mohamadnurrivaldyardin@gmail.com>
2022-07-14 22:35:47 +0700

    update

--- model/admin.go
+++ model/admin.go
@@ -1,14 +1,14 @@
 package model
 
 type Admin struct {
-	Id         int    `gorm:"primaryKey" json:"Id"`
-	Nama       string `json:"Nama"`
-	Email      string `json:"Email"`
-	Password   string `json:"Password"`
-	Kost_Id    int    `json:"_"`
-	Pemilik_Id int    `json:"_"`
-	// Kost       Kost    `json:"Kost" gorm:"foreignKey:Kost_Id_;references:Id"`
-	// Pemilik    Pemilik `json:"Pemilik" gorm:"foreignKey:Pemilik_Id;references:Id"`
+	Id         int     `gorm:"primaryKey" json:"Id"`
+	Nama       string  `json:"Nama"`
+	Email      string  `json:"Email"`
+	Password   string  `json:"Password"`
+	Kost_Id    int     `json:"_"`
+	Pemilik_Id int     `json:"_"`
+	Kost       Kost    `json:"Kost" gorm:"foreignKey:Kost_Id_;references:Id"`
+	Pemilik    Pemilik `json:"Pemilik" gorm:"foreignKey:Pemilik_Id;references:Id"`
 }
 
 type AdminResponse struct {
